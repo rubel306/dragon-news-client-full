a88b7e2e5697b087565cc954c7901f6776fb14af
RubelHasan <rubelhasan306@gmail.com>
2023-10-02 21:27:36 +0100

    install moment js for date format, also install react-fast-marquee for news ticker

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import HeaderNavbar from "../Navbar/HeaderNavbar";
+import logo from "../../../../public/logo.png";
+import moment from "moment/moment";
 
 const Header = () => {
   return (
     <div>
-      <h2>this is header </h2>
+      <div className="text-center">
+        <img src={logo} alt="" />
+        <p>Journalism Without Fear or Favour</p>
+        <p>{moment().format("dddd, MMMM, D, YYYY ")}</p>
+      </div>
       <HeaderNavbar></HeaderNavbar>
     </div>
   );
--- src/pages/Shared/Navbar/HeaderNavbar.jsx
+++ src/pages/Shared/Navbar/HeaderNavbar.jsx
@@ -3,22 +3,27 @@ import { Button, Container, Nav, NavDropdown } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
 import { Link } from "react-router-dom";
 import { FaUserAlt } from "react-icons/fa";
+import "./HeaderNavbar.css";
 
 const HeaderNavbar = () => {
   return (
     <div>
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+      <Navbar
+        collapseOnSelect
+        expand="lg"
+        className="bg-body-tertiary header-menu"
+      >
         <Container>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mx-auto">
-              <Link to="/">Home </Link>
+              <Link to="/">Home</Link>
               <Link to="/about">About </Link>
               <Link to="/career">Career</Link>
             </Nav>
             <Nav>
-              <p>
-                <FaUserAlt size="30px"></FaUserAlt>{" "}
+              <p className="mb-0">
+                <FaUserAlt size="30px" className="me-2"></FaUserAlt>
               </p>
               <Button variant="dark">Dark</Button>
             </Nav>
