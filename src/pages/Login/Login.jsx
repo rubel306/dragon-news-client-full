3fe8f1ffbdb854af6ced99eb1510bc9d1e566e69
RubelHasan <rubelhasan306@gmail.com>
2023-10-03 22:01:05 +0100

    User Sign in / Login done successfully

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,34 @@
 import { Button, Form } from "react-bootstrap";
 import "./Login.css";
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { AuthContext } from "../../providers/AuthProviders";
 
 const Login = () => {
+  const { signIn } = useContext(AuthContext);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    //user sign/login function
+
+    signIn(email, password)
+      .then((result) => {
+        const loggedUser = result.user;
+        console.log(loggedUser);
+        console.log("User Logged in successfully", loggedUser.email);
+        form.reset();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
   return (
     <div className="login bg-white w-75 p-5 border mx-auto">
       <h2 className="text-center">Login your account</h2>
-      <Form>
+      <Form onSubmit={handleLogin}>
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
           <Form.Control type="email" name="email" placeholder="Enter email" />
@@ -20,7 +42,7 @@ const Login = () => {
           />
         </Form.Group>
         <div className="d-grid gap-2">
-          <Button variant="dark" size="lg">
+          <Button type="submit" variant="dark" size="lg">
             Log In
           </Button>
         </div>
